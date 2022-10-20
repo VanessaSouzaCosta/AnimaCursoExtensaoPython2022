c7fcefb5108c07abbaa266be4df598a3635af4e5
Vanessa Souza Costa <109699478+VanessaSouzaCosta@users.noreply.github.com>
2022-10-20 00:53:00 +0000

    Aprendi a criar variáveis e exibir na tela!

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # Meu primeiro projeto Python!
+
 '''
 comentário em bloco
 '''
+
 # Print() = comando de saída
 print("Alô mundo!")
+
+# Quando quiser guardar uma String (frase)
+nome = "Vanessa Souza"
+
+# Quando quiser guardar um número inteiro
+idade = 34
+
+# Exibir a variável nome
+print(nome)
